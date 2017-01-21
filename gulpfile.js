e4bfc8dfe8e51f36d1b32f90e57d220fd5c5591b
wolfson <wolfson@bracketedrebels.com>
2017-01-21 23:45:28 +0700

    building process improvements

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,23 @@
 'use strict';
 
-const gulp = require('gulp');
-const jasmine = require('gulp-jasmine');
-const tsProject = require('gulp-typescript').createProject('tsconfig.json');
 const del = require('del');
+const gulp = require('gulp');
+const merge = require('merge2');
+const jasmine = require('gulp-jasmine');
+const tsprojectDev = require('gulp-typescript').createProject('tsconfig.dev.json');
+const tsprojectDist = require('gulp-typescript').createProject('tsconfig.dist.json');
 
-gulp.task('internal:tests', ['internal:compile'], () => 
-    gulp.src('tmp/**/*.spec.js')
-    .pipe(jasmine())
-);
-
-gulp.task('internal:compile', () => 
-    gulp.src('src/**/*.ts')
-    .pipe(tsProject()).js
+gulp.task('internal:compile:dev', () => 
+    tsprojectDev.src()
+    .pipe(tsprojectDev()).js
     .pipe(gulp.dest('tmp'))
 );
+gulp.task('internal:compile:dist', () => {
+    let project = tsprojectDist.src().pipe(tsprojectDist());
+    let dist = gulp.dest('dist');
+    return merge([ project.dts.pipe(dist), project.js.pipe(dist) ]);
+});
+gulp.task('internal:tests', ['internal:compile:dev'], () => gulp.src('tmp/**/*.spec.js').pipe(jasmine()));
 
 gulp.task('test', ['internal:tests'], () => del('tmp'));
+gulp.task('dist', ['internal:tests', 'internal:compile:dist'], () => del('tmp'));
