7984df6b8a307308db32521edd34e1bf0b453b39
wolfson <wolfson@bracketedrebels.com>
2017-01-22 13:32:39 +0700

    api docs improved

--- src/erdomain.class.ts
+++ src/erdomain.class.ts
@@ -4,67 +4,86 @@ import * as assign from 'object-assign';
 
 
 export class ERDomain {
-    /** Registers new link type.
-     *  @argument mark - link type name. Must be unique among all of links types within the current domain.
-     *  @argument options - link type properties.
+    /** 
+     * Registers new link type.
+     * @argument mark - link type name. Must be unique among all of links types within the current domain.
+     * @argument options - link type properties.
+     * @returns current ERDomain instance
      */
-    public addLinkType(mark: string, options?: ERDomainLinkTypeOptions): void {
+    public addLinkType(mark: string, options?: ERDomainLinkTypeOptions): ERDomain {
         this.validateLinkTypeExistence(mark, true);
         this.linkTypes[mark] = assign({}, { mutual: false, transitive: false }, options); // immutable assignation
+        return this;
     }
 
-    /** Updates properties of already registered link type.
-     *  @argument mark - link type name.
-     *  @argument options - new link type properties. Specified will override old ones, others will stay unchanged.
+    /** 
+     * Updates properties of already registered link type.
+     * @argument mark - link type name.
+     * @argument options - new link type properties. Specified will override old ones, others will stay unchanged.
+     * @returns current ERDomain instance
      */
-    public editLinkType(mark: string, options: ERDomainLinkTypeOptions): void {
+    public editLinkType(mark: string, options: ERDomainLinkTypeOptions): ERDomain {
         this.validateLinkTypeExistence(mark, false);
         this.linkTypes[mark] = assign({}, this.linkTypes[mark], options); // immutable assignation
+        return this;
     }
 
-    /** Get full list of link types names registered within the domain. */
+    /**
+     * Get full list of link types names registered within the domain.
+     * @return list of all link types names registered
+     */
     public getLinkTypes(): string[] {
         return Object.keys(this.linkTypes);
     }
 
-    /** Get registered link type options.
-     *  @argument mark - link type name
-     *  @argument silent - if true, no exception will be throwed in case of specified link type does not exist.
+    /** 
+     * Get registered link type options.
+     * @argument mark - link type name
+     * @argument silent - if true, no exception will be throwed in case of specified link type does not exist.
+     * @returns link type details or null if lik type is not registered
      */
     public getLinkTypeInfo(mark: string, silent = false): ERDomainLinkTypeOptions | null {
         if (!silent) { this.validateLinkTypeExistence(mark); }
         return this.linkTypes[mark] || null;
     }
 
-    /** Detecting, whether current domain has a registered link type with specified mark or not.
-     *  @argument mark - link type name
+    /**
+     * Detecting, whether current domain has a registered link type with specified mark or not.
+     * @argument mark - link type name
+     * @returns true if link type registered, false otherwise
      */
     public hasLinkType(mark: string): boolean {
         return mark in this.linkTypes;
     }
 
-    /** Removes registered link type.
-     *  @argument mark - registered link type name.
-     *  @argument consistent - if true, type removing is consistent. It means, that all links of this type will be also removed.
+    /** 
+     * Removes registered link type.
+     * @argument mark - registered link type name.
+     * @argument consistent - if true, type removing is consistent. It means, that all links of this type will be also removed.
+     * @returns current ERDomain instance
      */
-    public removeLinkType(mark: string, consistent = true): void {
+    public removeLinkType(mark: string, consistent = true): ERDomain {
         this.removeLinkTypeIfRequired(mark, consistent);
+        return this;
     }
 
-    /** Connect two entities by link of specified type and assign specified value on this link.
-     * 
-     *  @argument entities - pair of strings that identify entities
-     *  @argument linkType - name of link type, that will be used to link entities
-     *  @argument value - value, to assign on created link
+    /** 
+     * Connect two entities by link of specified type and assign specified value on this link.
+     * @argument entities - pair of strings that identify entities
+     * @argument linkType - name of link type, that will be used to link entities
+     * @argument value - value, to assign on created link
+     * @returns current ERDomain instance
      */
-    public link(linkType: string, entities: [string, string], value?: any): void {
+    public link(linkType: string, entities: [string, string], value?: any): ERDomain {
         this.linkEntities(linkType, entities, value);
+        return this;
     }
 
     /**
      * Gives you an information whether entities has link of specified type. If no type specified
      * it looks for link of any type between specified entities.
-     * 
+     * @argument entities - pair of entities names to test.
+     * @argument linkType - name of link that chold connect entitied.
      * @returns whether pair if antites connected via link of specified type.
      */
     public areLinked(entities: [string, string], linkType?: string): boolean {
@@ -77,63 +96,85 @@ export class ERDomain {
      * if no type specified) between them. If specified single entity, removes all 
      * links of specified type (or just all links if no type specified) that specified 
      * entity is source for.
-     * 
-     *  @argument linkType - name of link type, links of which will be removed.
-     *  @argument entities - list of entities or single entity to unlink.
+     * @argument linkType - name of link type, links of which will be removed.
+     * @argument entities - list of entities or single entity to unlink.
+     * @returns current ERDomain instance
      */
-    public unlink(entities: string | [string, string], linkType?: string): void {
+    public unlink(entities: string | [string, string], linkType?: string): ERDomain {
         this.unlinkEntities(entities, linkType);
+        return this;
     }
 
     /**
      * Removes all links of specified type (or simply all links if no type specified)
      * within the domain.
-     * 
      * @argument linkType - name of link type.
+     * @returns current ERDomain instance
      */
-    public unlinkAll(linkType?: string): void {
+    public unlinkAll(linkType?: string): ERDomain {
         this.removeAllLinksOfType(linkType);
+        return this;
     }
 
-    /** Add entity to domain.
-     *  @argument mark - entity identifier. Must be unique among all of link types within the current domain.
-     *  @argument value - value to assign to entity.
+    /**
+     * Add entity to domain.
+     * @argument mark - entity identifier. Must be unique among all of link types within the current domain.
+     * @argument value - value to assign to entity.
+     * @returns current ERDomain instance
      */
-    public addEntity(mark: string, value?: any): void {
+    public addEntity(mark: string, value?: any): ERDomain {
         this.validateEntityExistence(mark, true);
         this.graph.setNode(mark, value);
+        return this;
     }
 
-    /** Update entity assigned value.
-     *  @argument mark - entity identifier.
-     *  @argument value - new value to assign to entity.
+    /**
+     * Update entity assigned value.
+     * @argument mark - entity identifier.
+     * @argument value - new value to assign to entity.
+     * @returns current ERDomain instance
      */
-    public editEntity(mark: string, value?: any): void {
+    public editEntity(mark: string, value?: any): ERDomain {
         this.validateEntityExistence(mark, false);
         this.graph.setNode(mark, value);
+        return this;
     }
 
-    /** Detect whether an entity with specified mark exists within the domain. */
+    /**
+     * Detect whether an entity with specified mark exists within the domain.
+     * @argument mark - entity name.
+     * @returns whether domain has an entity with specified name or not.
+     */
     public hasEntity(mark: string): boolean {
         return this.graph.hasNode(mark);
     }
 
-    /** Remove entity from domain. Removing entity unlinks all related entities from it.
-     *  @argument mark - entity identifier.
+    /**
+     * Remove entity from domain. Removing entity unlinks all related entities from it.
+     * @argument mark - entity identifier.
+     * @returns current ERDomain instance.
      */
-    public removeEntity(mark: string): void {
+    public removeEntity(mark: string): ERDomain {
         this.graph.removeNode(mark);
+        return this;
     }
 
-    /** Acquire existed entity details.
-     *  @argument mark - entity name
-     *  @argument silent - if true, no exception will be throwed in case of specified entity does not exist.
+    /**
+     * Acquire existed entity details.
+     * @argument mark - entity name
+     * @argument silent - if true, no exception will be throwed in case of specified entity does not exist.
+     * @returns specified entity details or null if no such an entity found.
      */
-    public getEntityDetails(mark: string, silent = false): any | void {
+    public getEntityDetails(mark: string, silent = false): any | null {
         if (!silent) { this.validateEntityExistence(mark, false); }
         return this.graph.node(mark);
     }
 
+    /**
+     * Creating a domain of entites and connections between them. Graph to store domain can be specified,
+     * otherwise default store will be created.
+     * @argument store - graph for storing the domain.
+     */
     constructor(store?: Graph) {
         this.graph = store || new Graph({ directed: true, multigraph: true });
     }
