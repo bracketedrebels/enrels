4cfffb7880491970be4ce0e8ffd284a721c86d90
wolfson <wolfson@bracketedrebels.com>
2017-02-26 00:17:59 +0700

    refactor(build): gulpfile tasks actualized

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,19 @@ const del = require('del');
 const gulp = require('gulp');
 const merge = require('merge2');
 const jasmine = require('gulp-jasmine');
-const tsprojectDev = require('gulp-typescript').createProject('tsconfig.dev.json');
-const tsprojectDist = require('gulp-typescript').createProject('tsconfig.dist.json');
+const tsproject = require('gulp-typescript').createProject('tsconfig.json');
 
 gulp.task('internal:compile:dev', () => 
-    tsprojectDev.src()
-    .pipe(tsprojectDev()).js
+    gulp.src('src/**/*.ts')
+    .pipe(tsproject()).js
     .pipe(gulp.dest('tmp'))
 );
 gulp.task('internal:compile:dist', () => {
-    let project = tsprojectDist.src().pipe(tsprojectDist());
+    let project = gulp.src(['src/**/*.ts', '!src/**/*.spec.ts']).pipe(tsproject());
     let dist = gulp.dest('dist');
     return merge([ project.dts.pipe(dist), project.js.pipe(dist) ]);
 });
 gulp.task('internal:tests', ['internal:compile:dev'], () => gulp.src('tmp/**/*.spec.js').pipe(jasmine()));
 
 gulp.task('test', ['internal:tests'], () => del('tmp'));
-gulp.task('dist', ['internal:tests', 'internal:compile:dist'], () => del('tmp'));
+gulp.task('dist', ['internal:compile:dist']);
